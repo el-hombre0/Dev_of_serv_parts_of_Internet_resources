164c4818963ccc2a0f711a636e3a4768c101fffd
Stanislav E <evendot@yandex.ru>
2022-09-22 23:13:13 +0300

    Practice 2 is ready

---
+++ Practice2/WebService/index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Drawer</title>
+    <link rel="stylesheet" href="./style.css">
+</head>
+<body>
+    <?php
+        $articles = [
+            [
+                'title' => 'Круг',
+                'content' => '<circle cx="102" cy="102" r="100" fill="rgb(255,0,0)" stroke-width="1" stroke="rgb(0,0,0)"/>'
+            ],
+            [
+                'title' =>  'Квадрат',
+                'content' => '<rect width="200" height="200" fill="rgb(255,128,0)" stroke-width="1" stroke="rgb(0,0,0)"/>'
+            ],
+            [
+                'title' =>  'Прямоугольник',
+                'content' => '<rect width="400" height="200" fill="rgb(255,255,0)" stroke-width="1" stroke="rgb(0,0,0)"/>'
+            ],
+            [
+                'title' =>  'Эллипс',
+                'content' => '<ellipse cx="100" cy="50" rx="100" ry="50" fill="rgb(0,255,0)" stroke-width="1" stroke="rgb(0,0,0)"/>'
+            ]
+        ];
+    ?>
+    <!-- Выводим меню -->
+    <a class="mainpage" href="/">Главная</a>
+    <br>
+    <h2>Упражнение 1</h2>
+    
+    <?php foreach($articles as $id => $article): ?>
+        <a href="/index.php?id=<?= $id ?>"><?= $article['title'] ?></a>
+        <br>
+    <?php endforeach; ?>
+    
+    <?php
+    // Если id нет в URL - отображаем главную страницу
+    if(!isset($_GET['id']))
+        echo '<h1>Главная</h1>';
+
+    // Если id есть, но нет фигуры с таким id - показываем ошибку
+    elseif(!isset($articles[$_GET['id']]))
+        echo '<h1>Ошибка: страница не существует.</h1>';
+
+    // Если id есть и статья с таким id существует - выводим статью
+    else
+    {
+        $article = $articles[$_GET['id']];
+
+        echo '<h1>' . $article['title'] . '</h1>';
+        echo '<svg>' . $article['content'] . '</svg>';
+    }
+    ?>
+    <h2>Упражнение 2</h2>
+    <a href="./shell_sort.php">Сортировка Шелла</a>
+
+    <h2>Упражнение 3</h2>
+    <a href="./admin_page.php">Информационно-административная страница о сервере</a>
+</body>
+</html>
---
+++ Practice2/WebService/shell_sort.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css" />
+    <title>Сортировка Шелла</title>
+</head>
+<body>
+    <a class="mainpage" href="./index.php">Главная</a>
+<?php
+    function array_fill_rand($limit, $min=true, $max=true) 
+    {
+        $limit = (int)$limit;
+        $numbers = array();
+        
+        if ($min !== false && $max !== false)
+        {
+            $min = (int)$min;
+            $max = (int)$max;
+            for ($i=0; $i<$limit; $i++)
+            {
+                $numbers[$i] = rand($min, $max);
+            }
+        }
+        else
+        {
+            for ($i=0; $i<$limit; $i++)
+            {
+                $numbers[$i] = rand();
+            }
+        }
+        
+        return $numbers;
+    }
+    $numbers = array_fill_rand(50, 0, 100);
+    // // $numbers = [21, 25, 100, 1, 3, 2, 34, 853, 298, 4, 98, 89, 77];
+    echo '<pre>';
+    print_r($numbers);
+    echo '</pre>';
+
+    function shell_Sort($my_array)
+    {
+        $x = round(count($my_array)/2);
+        while($x > 0)
+        {
+            for($i = $x; $i < count($my_array);$i++){
+                $temp = $my_array[$i];
+                $j = $i;
+                while($j >= $x && $my_array[$j-$x] > $temp)
+                {
+                    $my_array[$j] = $my_array[$j - $x];
+                    $j -= $x;
+                }
+                $my_array[$j] = $temp;
+            }
+            $x = round($x/2.2);
+        }
+        return $my_array;
+    }
+
+    $new_array = shell_Sort($numbers);
+    echo '<pre>';
+    print_r($new_array);
+    echo '</pre>';
+?>
+    
+</body>
+</html>
