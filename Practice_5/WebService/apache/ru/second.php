c02890be538164a89c922ff3d26256f3dd9c6cbb
Stanislav E <evendot@yandex.ru>
2022-12-02 10:16:31 +0300

    Added ru version after erroneous deletion

---
+++ Practice_5/WebService/apache/ru/second.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html>
+<head>
+   <meta http-equiv="Content-Type" content="text/html; charset=windows-1251" />
+   <link rel="stylesheet" href="../css/<?php echo $_SESSION['theme']; ?>.css" type="text/css">
+
+   <title>Ваше сообщение успешно отправлено</title>
+</head>
+ 
+<body>
+</body>
+</html>
+<?php
+   $back = "<p><a href=\"javascript: history.back()\">Вернуться назад</a></p>";
+ 
+   if(!empty($_POST['name']) and !empty($_POST['phone']) and !empty($_POST['mail']) 
+   and !empty($_POST['message'])){
+      $name = trim(strip_tags($_POST['name']));
+      $phone = trim(strip_tags($_POST['phone']));
+      $mail = trim(strip_tags($_POST['mail']));
+      $message = trim(strip_tags($_POST['message']));
+ 
+      mail('evendot@yandex.ru', 'Письмо с Портфолио', 
+      'Вам написал: '.$name.'<br />Его номер: '.$phone.'<br />Его почта: '.$mail.'<br />
+      Его сообщение: '.$message,"Content-type:text/html;charset=windows-1251");
+ 
+      echo "Ваше сообщение успешно отправлено!<Br> Вы получите ответ в 
+      ближайшее время<Br> $back";
+ 
+      exit;
+   } 
+   else {
+      echo "Для отправки сообщения заполните все поля! $back";
+      exit;
+   }
+?>
